1eca1c3c6f6c0abd1dec22d1f602c569bda15962
Tolben <vitek.pasek@gmail.com>
2025-10-21 02:36:55 +0200

    chore(schema): split from umbrella

---
+++ schema/001_table.sql
@@ -0,0 +1,8 @@
+-- Auto-generated from schema-map.psd1 on 2025-10-21T02:32:05
+-- table: payment_logs
+CREATE TABLE IF NOT EXISTS payment_logs (
+  id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  payment_id BIGINT UNSIGNED NOT NULL,
+  log_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  message TEXT NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
